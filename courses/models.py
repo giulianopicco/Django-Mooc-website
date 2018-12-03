4c8423bae6467e528be2a732047700cb21f415d0
giulianopicco <giulianopicco@gmail.com>
2018-12-03 00:36:31 -0300

    add new pages for steps

--- courses/models.py
+++ courses/models.py
@@ -7,3 +7,17 @@ class Course(models.Model):
 
     def __str__(self):
         return '{}'.format(self.title)
+
+
+class Step(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    content = models.TextField(blank=True, default='')
+    order = models.IntegerField(default=0)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['order',]
+    
+    def __str__(self):
+        return self.title
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from .models import Course
+from .models import Course, Step
 
 
 
@@ -10,3 +10,11 @@ def courses(request):
     output = '\n'.join([str(course) for course in courses])
     # return HttpResponse(courses)
     return render(request, 'courses/course_list.html', {'courses':courses})
+
+def course_detail(request, pk):
+    c = get_object_or_404(Course, pk=pk)
+    return render(request, 'courses/detail.html ', {'course':c,})
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(request, 'courses/step_detail.html', {'step':step})
